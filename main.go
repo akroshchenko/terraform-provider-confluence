87c2619a231ee152902b8a7274c23fddf9565e83
drfaust92 <ilia.lazebnik@gmail.com>
2023-09-29 08:15:48 +0300

    bump deps

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/chesshacker/terraform-provider-confluence/confluence"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: confluence.Provider})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		Debug:        debugMode,
+		ProviderFunc: confluence.Provider,
+	}
+
+	plugin.Serve(opts)
 }
